eaed9ae790dbb77fb4b8d544cb34e52442525d00
Alessandro Ebranati <alessandro.ebranati@gmail.com>
2018-02-07 22:14:51 +0100

    Sistemazione commenti in ita/eng

--- Trentini DOP - Space Pressure/deliverables/Team_Trentini_DOP_main.py
+++ Trentini DOP - Space Pressure/deliverables/Team_Trentini_DOP_main.py
@@ -1,19 +1,66 @@
-##### Librerie #####
+""" #########################################################################################
+
+    ASTROPI 2018 - MISSION "SPACE LAB" (Life in Space): GRUPPO I TRENTINI DOP (Italy)   
+
+Abstract IT: I sensori del Sense HAT sono monitorati in continuazione, in particolare per
+             quanto riguarda i parametri pressione e temperatura dell'aria interna alla ISS
+Abstract EN: Sense HAT sensors are monitored continuously, in particular the pressure and
+             the temperature of the air inside the ISS
+
+Gruppo 'I Trentini DOP' 2018 @ CoderDojo Trento, Italy:
+ - Matteo Nardin
+ - Lorenzo Berlanda
+ - Mattia Tomasi
+ - Stefano Libardi
+
+
+IT: Parametri utilizzati nel programma:
+PREFISSO_NOME_FILE            # prefisso nome file CSV
+FREQUENZA_SCRITTURA           # frequenza scrittura su file (in numero di righe)
+INTERVALLO_MISURAZIONI        # frequenza consultazione Sense HAT (in sec)
+INTERVALLO_CAMPIONAMENTO      # frequenza raccolta dati (in sec)
+
+EN: Config parameters used in the code:
+PREFISSO_NOME_FILE            # CSV file name prefix
+FREQUENZA_SCRITTURA           # writing to file frequency (in number of lines)
+INTERVALLO_MISURAZIONI        # data reading frequency (in sec)
+INTERVALLO_CAMPIONAMENTO      # data collection frequency (in sec)
+
+
+N.B. IT: Non e' necessario trascrivere nel codice le righe TLE della ISS: tutto il processamento e' 
+fatto dopo il volo; tutto quello di cui ha bisogno il programma e' un ora di sistema ben regolata
+(per ricostruire l'esposizione della ISS al sole dal timestamp dei dati).
+
+N.B. EN: No need to insert updated TLE entries: all processing will be made off-line after the flight;
+all we need is a well-synced system time (in order to reconstruct exposure to the sun of the ISS
+given the data timestamp).
+
+""" ###########################################################################
+
+####################  Import / Import  ####################
+
+# Librerie esterne / External libraries
 from datetime import datetime, timedelta
 from sense_hat import SenseHat
 from time import sleep
 from threading import Thread
 import subprocess
+
+# Moduli fatti in casa / Homemade modules
 from Team_Trentini_DOP_support import intro_video, mostra_salvataggio, mostra_avanzamento
 
-##### Configurazione del Logging #####
-NOME_TEAM = "Trentini DOP - Space Pressure"
-PREFISSO_NOME_FILE = "TDOP"
-FREQUENZA_SCRITTURA = 20
-INTERVALLO_MISURAZIONI=0.01
-INTERVALLO_CAMPIONAMENTO=1
 
-##### Funzioni #####
+#################### Configurazione / Configuration ####################
+
+PREFISSO_NOME_FILE = "TDOP"       # prefisso nome file CSV / CSV file name prefix
+FREQUENZA_SCRITTURA = 20          # frequenza scrittura su file (in numero di righe) / writing to file frequency (in number of lines)
+INTERVALLO_MISURAZIONI = 0.01     # frequenza consultazione Sense HAT (in sec) / data reading frequency (in sec)
+INTERVALLO_CAMPIONAMENTO = 1      # frequenza raccolta dati (in sec) / data collection frequency (in sec)
+
+
+#################### Funzioni / Functions ####################
+
+# crea il file ed aggiunge l'intestazione / create the file and insert the header
 def preparazione_file(nome_file):
     intestazione = []
     intestazione.append("ora")
@@ -29,7 +76,7 @@ def preparazione_file(nome_file):
     with open(nome_file, "w") as f:
         f.write(",".join(str(colonna) for colonna in intestazione)+ "\n")
 
-
+# legge i dati dal sensore e la temperatura della CPU / read the sensor data and the CPU temperature
 def leggi_dati_sensore():
     riga_di_dati = []
     riga_di_dati.append(datetime.now())
@@ -60,7 +107,7 @@ def leggi_dati_sensore():
 
     return riga_di_dati
 
-
+# campiona i dati con una frequenza data / samples data with a given frequency
 def raccolta_temporizzata():
     ora_ultimo_campionamento = datetime.now()
     while True:
@@ -73,25 +120,31 @@ def raccolta_temporizzata():
         sleep(INTERVALLO_MISURAZIONI)
 
 
-##### Main Program #####
-sense = SenseHat()
+#################### Programma principale / Main program ####################
 
-#----------------- video introduttivo / opening animation -------------------------------------
+sense = SenseHat()
+pila_di_dati = [] # buffer utilizzato per raccogliere i dati da scrivere su file / buffer used to collect data that has to be written in the file
+
+# IT: chiamata all'animazione introduttiva
+# EN: opening animation call
 
 print("Animazione introduttiva")
-intro_video() # chiamata all'animazione introduttiva / opening animation call
+intro_video()
 
-pila_di_dati = []
-
+# IT: richiama la funzione che crea il file con la riga di intestazione
+# EN: call to the function that creates the files with the header
 nome_file_dati = PREFISSO_NOME_FILE+"_"+(datetime.now().strftime("%Y-%m-%d_%H.%M.%S"))+".csv"
-
 preparazione_file(nome_file_dati)
 
+# IT: lancio thread per raccolta dati sensori
+# EN: start the thread for the sensor data gathering
 Thread(target = raccolta_temporizzata).start()
 
 try:
     while True:
         if len(pila_di_dati) >= FREQUENZA_SCRITTURA:
+            # IT: se sono state raccolte abbastanza misure, scrive su file
+            # EN: when enough measures have been collected, write them to file
             print("Scrivo sul file...")
             with open(nome_file_dati, "a") as f:
                 for line in pila_di_dati:
@@ -99,8 +152,12 @@ try:
             pila_di_dati = []
             mostra_salvataggio()
         else:
+            # IT: mostra il riempimento del buffer sulla matrice di LED
+            # EN: shows the filling of the buffer on the LED matrix
             mostra_avanzamento(len(pila_di_dati)/FREQUENZA_SCRITTURA)
 
+# IT: se premo CTRL+C esce dal programma e svuota il buffer
+# EN: when we hit CTRL+C, it terminate the program by emptying the buffer
 except KeyboardInterrupt:
     print('Esecuzione interrotta!')
     print("Scrivo gli ultimi dati sul file...")
--- Trentini DOP - Space Pressure/deliverables/Team_Trentini_DOP_support.py
+++ Trentini DOP - Space Pressure/deliverables/Team_Trentini_DOP_support.py
@@ -1,17 +1,30 @@
 #############################################################
-# IT: animazioni introduttiva con bandiera e scritta mobile #
-# EN: opening animation with flag and scrolling banner      #
+"""
+IT: questo modulo raccoglie le animazione impiegate nel programma
+principale, all'avvio ed al momento del salvataggio dei dati su disco;
+realizza anche il disegno che mostra il caricamento progressivo del buffer
+
+EN: this module collects the animations used in the main program
+at the beginning as presentation and when saving data to disk;
+it also implements the drawing showing the progressive loading of the buffer
+"""
 #############################################################
 
 from sense_hat import SenseHat
 from time import sleep
 
+# IT: definiamo i colori che vengono usati in tutti i disegni
+# EN: let's define the colors that are used in all the drawings
 w=(255, 255, 255)  # bianco / white
 n=(  0,   0,   0)  # nero   / black
 r=(255,   0,   0)  # rosso  / red
 b=(  0,   0, 255)  # blu    / blue
 v=(  0, 153,   0)  # verde  / green
 g=(190, 190,   0)  # giallo / yellow
+
+
+# IT: animazione introduttiva: mostra la bandiera italiana e scritta scorrevole col nome del gruppo
+# EN: opening animation: it shows the italian flag and a scrolling banner with the name of the team
 
 def intro_video():
     s=SenseHat()
@@ -38,11 +51,14 @@ def intro_video():
     sleep(1)
 
 
+# IT: animazione mostrata durante il salvataggio dei dati su disco: una freccia mobile che punta all'icona di una cartella
+# EN: animation shown when saving data to disk: a moving arrow that points toward a folder icon
+
 def mostra_salvataggio():
     s=SenseHat()
     s.low_light = True
 
-    # messaggio di presentazione / presentation message
+    # freccia mobile / moving arrow
     s.show_message("-->", text_colour=v, scroll_speed=0.04)
 
     # disegno cartella / folder image
@@ -60,15 +76,24 @@ def mostra_salvataggio():
 
     sleep(1)
 
+
+# IT: disegno che mostra il caricamento progressivo del buffer: la funzione viene richiamata 
+#     dal programma principale indicando una percentuale di riempimento; al suo interno viene 
+#     calcolato quanti dei 64 LED devono venire accesi e quanti vanno spenti
+# EN: drawing that shows the progressive loading of the buffer: the function is called by the
+#     main program that passes the filling percentage as argument; inside the body it is calculated
+#     how many out of the 64 LEDs must be turned on and how many off
+
 def mostra_avanzamento(percentuale):
     s=SenseHat()
     s.low_light = True
 
     avanzamento = []
-    pieni = int(percentuale * 64);
-    for i in range(pieni):
+    accesi = int(percentuale * 64);
+    spenti = 64 - accesi
+    for i in range(accesi):
         avanzamento.append(b)
-    for i in range(64 - pieni):
+    for i in range(spenti):
         avanzamento.append(n)
     
     s.set_pixels(avanzamento)
