61752071aca229a0975ca387c8b110b2a34b6c5d
Alessandro Ebranati <alessandro.ebranati@gmail.com>
2018-02-21 00:07:25 +0100

    aggiunte procedure per scalare le immagini, corretto algoritmo calcolo distanza

--- Team Lampone - ISS Orbit/versione_ok/postprocessing/main.py
+++ Team Lampone - ISS Orbit/versione_ok/postprocessing/main.py
@@ -44,9 +44,13 @@ SALVA_IMMAGINI_PUNTI_CHIAVE = False
 INTERATTIVO = False # mostrare o meno i plot interattivi
 # parametri PiCamera
 focale=3.60  # lunghezza focale (mm)  (v1: 3.60;  v2: 3.04)
-sensx=3760   # dimensione sensore asse x (in pixel) (v1: 3760;  v2:3680)
-resx=640     # risoluzione img x
-
+sensx=3.76   # dimensione sensore asse x (mm) (v1: 3.76;  v2:3.68)
+resx=640     # risoluzione img x (v1: 2592 x 1944 pixels; v2: 3280 x 2464 pixels)
+# per fare delle prove veloci, conviene ridurre le immagini: serve tenere traccia del
+# fattore di scala e dell'orientazione originale per sapere su quale dimensione scalare
+DIMENSIONE_RIDOTTA = 800 # misura massima in pixel (per ridurre il tempo di elaborazione)
+fattore_di_scala = 1.0 # se si riduce l'immagine, bisogna correggere il calcolo delle distanze
+orientazione = "LANDSCAPE" # "LANDSCAPE" oppure "PORTRAIT"
 
 #==========================================================================
 
@@ -58,6 +62,28 @@ def prendi_lista_immagini(folder):
     return [fn for fn in os.listdir(folder)
               if aspetto_file.match(fn)]
 
+def calcola_fattore_di_riduzione(immagine_campione):
+    scala = 1.0
+    orientaz = "LANDSCAPE"
+    imageAaltezzaPx, imageAlarghezzaPx = immagine_campione.shape[:2]
+    if ((imageAlarghezzaPx > DIMENSIONE_RIDOTTA) or (imageAaltezzaPx > DIMENSIONE_RIDOTTA)):
+        if (imageAlarghezzaPx > imageAaltezzaPx):
+            orientaz = "LANDSCAPE"
+            scala = imageAlarghezzaPx / DIMENSIONE_RIDOTTA
+        else:
+            orientaz = "PORTRAIT"
+            scala = imageAaltezzaPx / DIMENSIONE_RIDOTTA
+    print(imageAlarghezzaPx, imageAaltezzaPx)
+    return scala, orientaz
+
+def riduci_immagine(immagine):
+    if (fattore_di_scala > 1.0):
+        if (orientazione == "LANDSCAPE"):
+            immagine = imutils.resize(immagine, width=DIMENSIONE_RIDOTTA)
+        else:
+            immagine = imutils.resize(immagine, height=DIMENSIONE_RIDOTTA)
+    return immagine
+
 
 #=============================================================================
 
@@ -68,7 +94,10 @@ lista_imgs = prendi_lista_immagini(FOLDER_IMMAGINI)
 immagine_1 = lista_imgs[0]
 timestamp_1 = datetime.strptime(immagine_1,NOME_FILE_ASPETTO)
 imageA = cv2.imread(os.path.join(FOLDER_IMMAGINI, immagine_1))
-imageA = imutils.resize(imageA, width=800)
+fattore_di_scala, orientazione = calcola_fattore_di_riduzione(imageA)
+print("fattore_di_scala", fattore_di_scala)
+print("orientazione", orientazione)
+imageA = riduci_immagine(imageA)
 for file_immagine in lista_imgs[1:-1]:
     immagine_2 = file_immagine
     timestamp_2 = datetime.strptime(immagine_2,NOME_FILE_ASPETTO)
@@ -76,7 +105,7 @@ for file_immagine in lista_imgs[1:-1]:
     delta_t_sec = delta_t.seconds + delta_t.microseconds / 1000000
     print("Confronto %s e %s (Dt = %f s)..." % (immagine_1, immagine_2, delta_t_sec))
     imageB = cv2.imread(os.path.join(FOLDER_IMMAGINI, immagine_2))
-    imageB = imutils.resize(imageB, width=800)
+    imageB = riduci_immagine(imageB)
 
     # stitch the images together to create a panorama
     stitcher = MyStitcher()
@@ -101,8 +130,9 @@ for file_immagine in lista_imgs[1:-1]:
     print(distanza_px)
     
     print("Distanza espressa in metri:")
-    altezzaiss = 400000000  #altezza ISS (mm) TODO: da calcolare con PyEphem
-    distanza = (1/focale*(distanza_px*(sensx/resx/1000))*altezzaiss)/1000
+    altezzaiss = 408*1000 #altezza ISS (m) TODO: da calcolare con PyEphem
+#    distanza = (1/focale*(distanza_px*(sensx/resx/1000))*altezzaiss)/1000
+    distanza = (sensx*altezzaiss/focale)*(distanza_px/resx)*fattore_di_scala
     print(distanza)
     
     # stima velocità: teo, circa 27563 Km/h
